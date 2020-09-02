cd1e8931bd9e67aa66ab1a434d62d4ddd48908bb
Francis John Owen <fj.owen@live.dk>
2020-09-02 12:18:25 +0200

    ApprovedType class

---
+++ Planter API/Models/ApprovedType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Planter_API.Models
+{
+    public class ApprovedType
+    {
+        [Key]
+        public int ApprovedTypeID { get; set; }//PK
+        public string AType { get; set; }
+
+        //Relationships (Til Senere)
+
+        //PK ApprovedTypeID(ApprovedType) to FK_ApprovedTypeID(Plants)
+        //public List<Plant> Plants { get; set; }
+
+        //PK ApprovedTypeID(ApprovedType) to FK_ApprovedTypeID(Article)
+        //public List<Article> Article { get; set; }
+
+    }
+}
