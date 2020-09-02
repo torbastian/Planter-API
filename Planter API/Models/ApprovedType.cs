b984d58a6c6a00390f52c3146eae59f7e346b550
Francis John Owen <fj.owen@live.dk>
2020-09-02 12:53:06 +0200

    ApprovedType, Article

--- Planter API/Models/ApprovedType.cs
+++ Planter API/Models/ApprovedType.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Planter_API.Models
 {
---
+++ Planter API/Models/Article.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planter_API.Models
+{
+    public class Article
+    {
+        [Key]
+        public int ArticleID { get; set; }//PK
+        public string Text { get; set; }
+        public string Tips { get; set; }
+        
+        //FK ApprovedTypeID
+        public ApprovedType ApprovedType { get; set; }
+        [ForeignKey("ApprovedTypeID")]
+        public int ApprovedTypeID { get; set; }
+
+        //FK PlantID
+        //public Plants Plants { get; set; }
+        //[ForeignKey("PlantID")]
+        //public int PlantID { get; set; }
+
+        //Relationships (Til Senere)
+
+        //PK ArticleID(Article) TO FK_ArticleID(Comments)
+        //public List<Comment> Comments { get; set; }
+
+        //PK PlantID(Plants) TO FK_PlantID(Article)
+
+        //PK ApprovedTypeID(ApprovedType) TO FK_ApprovedTypeID(Article)
+    }
+}
