cdee5030d718c21eaa956f25a329545cdcfd3f90
Tor-Bastian Torh. Hansen <torh@pc-tec.local>
2020-09-02 08:13:59 +0200

    Add project files.

---
+++ Planter API/Models/PlanterContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Planter_API.Models
+{
+    public class PlanterContext : DbContext
+    {
+        public PlanterContext(DbContextOptions<PlanterContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            //Not sure we need this, but I'll keep it here for now
+            foreach(var relationship in modelbuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.SetNull;
+            }
+
+            base.OnModelCreating(modelbuilder);
+        }
+
+        //DBsets
+    }
+}
