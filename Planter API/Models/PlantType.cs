03aa66b35abbd4da5f3f8b38defd8215cde5213b
Tor-bastian Hansen <torbastianhansen@hotmail.com>
2020-09-02 10:25:33 +0200

    Oprettet PlantType

---
+++ Planter API/Models/PlantType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planter_API.Models
+{
+    public class PlantType
+    {
+        [Key]
+        public int PlantTypeID { get; set; }
+        public string PType { get; set; }
+
+        //Relationships (Til Senere)
+        //public List<Plant> Plants { get; set; }
+    }
+}
